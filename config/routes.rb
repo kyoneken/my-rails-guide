c28b4bcc5a5301214c1f59ffce17b85f6ca8256b
kyoneken <spry.car7536@fastmail.com>
2024-09-29 03:54:47 +0000

    7 CRUDの重要性

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :articles
 end
